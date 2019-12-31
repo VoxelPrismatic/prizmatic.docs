78da7c935b68f0daf1c21eb5c84059c90fd28c5c
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-31 14:24:42 -0600

    Update docs.js

--- script/docs.js
+++ script/docs.js
@@ -28,7 +28,7 @@ var docs_regex = [
             return "";
         }
     ], [
-        /\{\{cls\}\} (.+?) = (.+?)\(([\w\d*_, ]*)\)\n\n/gm,
+        /\{\{cls\}\} (.+?) = (.+?)\(([\w\d*_, \n]*)\)\n\n/gm,
         function(m, p1, p2, p3) {
             var st = `<div id="top"></div><div id="${p2}" class="head1">`;
             st += `#] ` + p2 + ` <span class="typ">{{cls}}</span>`;
