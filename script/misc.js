c22c813a910087dea9346ab3376a993b8192a60b
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-01-08 13:06:20 -0600

    Update misc.js

--- script/misc.js
+++ script/misc.js
@@ -134,7 +134,7 @@ function collapser(elem) {
     }
     var thing = elem.children;
     for(var child of thing) {
-        if(disp && !child.classList.includes("bogone"))
+        if(disp && !child.className.includes("bogone"))
             child.style.display = "block";
         else
             child.style.display = "none";
