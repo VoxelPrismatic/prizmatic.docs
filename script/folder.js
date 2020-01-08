eb03d2a53737b43c1417ced0a70f352b81e8a9f4
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-01-08 10:45:35 -0600

    Update folder.js

--- script/folder.js
+++ script/folder.js
@@ -1,6 +1,10 @@
 function folder_back() {
-    while(prev_pages.slice(-1)[0] == findHtml("this-here"))
-        prev_pages = prev_pages.slice(0, -1)
+    let this_here = findHtml("this-here");
+    var last_elem = prev_pages.slice(-1)[0];
+    while(prev_pages.join("") != "" && (last_elem == this_here || last_elem == undefined)) {
+        prev_pages = prev_pages.slice(0, -1);
+        last_elem = prev_pages.slice(-1)[0]
+    }
     id = prev_pages.slice(-1)[0];
     next_pages.push(id);
     prev_pages = prev_pages.slice(0, -1)
@@ -27,8 +31,12 @@ function folder_up() {
 }
 
 function folder_next() {
-    while(next_pages.slice(-1)[0] == findHtml("this-here"))
-        next_pages = next_pages.slice(0, -1)
+    let this_here = findHtml("this-here");
+    var last_elem = next_pages.slice(-1)[0];
+    while(next_pages.join("") != "" && (last_elem == this_here || last_elem == undefined)) {
+        next_pages = next_pages.slice(0, -1);
+        last_elem = next_pages.slice(-1)[0]
+    }
     var stuffs = find("nav").children;
     var stuff = [];
     for(thing in stuffs)
