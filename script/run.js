28e10f168c5c5077a5a0a9b12d1979bfe13ec1bd
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-01-27 15:43:36 -0600

    Update run.js

--- script/run.js
+++ script/run.js
@@ -1,6 +1,10 @@
 url = document.URL;
 if(url.includes("?")) {
-    loadUri(url.split("?")[1]);
+    try {
+        loadUri(url.split("?")[1]);
+    } catch(err) {
+        setHtml("page", findHtml("DOCS_/prizmatic.docs/doc/" + url.split("?")[1].split("#")[0]));
+    }
 } else {
     loadDoc("/prizmatic.docs/doc/index.txt");
 }
